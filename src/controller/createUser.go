ca5f04e6ec01face60072b41834c46a0b1f1ccfe
Eduardo Lima <dlima78@gmail.com>
2025-10-14 10:40:17 -0300

    Adicionar logs

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,21 +1,41 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/dlima78/gocourse/src/configuration/logger"
 	"github.com/dlima78/gocourse/src/configuration/validation"
 	"github.com/dlima78/gocourse/src/model/request"
+	"github.com/dlima78/gocourse/src/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("About to start user application",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info,", err,
+			zap.String("journey", "createUser"))
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
+
 }
