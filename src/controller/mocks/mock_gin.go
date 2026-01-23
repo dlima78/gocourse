ec757a9521f11ec417696d277fb4ea7321b4bd3a
Eduardo <dlima78@hotmail.com>
2026-01-23 17:16:09 -0300

    Adicionar mock gin

---
+++ src/controller/mocks/mock_gin.go
@@ -0,0 +1,29 @@
+package mock_user_controller
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetTestingGinContext(recorder *httptest.ResponseRecorder) *gin.Context {
+	gin.SetMode(gin.TestMode)
+	ctx, _ := gin.CreateTestContext(recorder)
+	ctx.Request = &http.Request{
+		Header: make(http.Header),
+		URL:    &url.URL{},
+	}
+	return ctx
+}
+
+func MakeRequest(c *gin.Context, param gin.Params, u url.Values, method string, body io.ReadCloser) {
+	c.Request.Method = method
+	c.Request.Header.Set("Content-Type", "application/json")
+	c.Params = param
+
+	c.Request.Body = body
+	c.Request.URL.RawQuery = u.Encode()
+}
