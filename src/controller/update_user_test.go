2b014cd082f20675e081f6d9aab7f9b196e28816
Eduardo <dlima78@hotmail.com>
2026-01-26 16:22:38 -0300

    Adicionar TestUpdateUserController_NotFound

--- src/controller/update_user_test.go
+++ src/controller/update_user_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	rest_err "github.com/dlima78/gocourse/src/configuration"
 	mock_user_controller "github.com/dlima78/gocourse/src/controller/mocks"
 	"github.com/dlima78/gocourse/src/model"
 	"github.com/gin-gonic/gin"
@@ -42,6 +43,7 @@ func TestUpdateUserController_Success(t *testing.T) {
 	assert.Equal(t, http.StatusOK, recorder.Code)
 
 }
+
 func TestUpdateUserController_ValidationError(t *testing.T) {
 	recorder := httptest.NewRecorder()
 	context := mock_user_controller.GetTestingGinContext(recorder)
@@ -63,5 +65,33 @@ func TestUpdateUserController_ValidationError(t *testing.T) {
 
 	assert.Equal(t, http.StatusBadRequest, recorder.Code)
 	mockService.AssertNotCalled(t, "UpdateUserService")
+
+}
+
+func TestUpdateUserController_NotFound(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	context := mock_user_controller.GetTestingGinContext(recorder)
+
+	id := "507f1f77bcf86cd799439011"
+	body := `{"name":"Novo Nome","age":31}`
+
+	mockService := new(mock_user_controller.MockUserService)
+	params := gin.Params{gin.Param{Key: "userId", Value: id}}
+	mock_user_controller.MakeRequest(
+		context,
+		params,
+		nil,
+		"PUT",
+		io.NopCloser(strings.NewReader(body)))
+
+	errResp := rest_err.NewBadRequestError("user not found")
+
+	mockService.On("UpdateUserService", id, mock.Anything).Return(errResp)
+
+	controller := NewUserController(mockService)
+	controller.UpdateUser(context)
+
+	mockService.AssertExpectations(t)
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
 
 }
