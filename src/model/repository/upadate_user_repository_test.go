43b5c4b41f709d5310d1a0bbcde711313db940bf
Eduardo <dlima78@hotmail.com>
2026-01-29 18:25:15 -0300

    Adicionar testes de id invalido para update user repository

--- src/model/repository/upadate_user_repository_test.go
+++ src/model/repository/upadate_user_repository_test.go
@@ -37,3 +37,27 @@ func TestUserRepository_TestUpdateUser(t *testing.T) {
 	require.Equal(t, int8(47), updatedUser.GetAge(), "Expected age to match")
 
 }
+
+func TestUserRepository_TestUpdateUser_InvalidID(t *testing.T) {
+	setup := SetupMongoDB(t, "user_database_test")
+	defer setup.Cleanup()
+
+	originalValue := os.Getenv("MONGODB_USER_DB")
+	os.Setenv("MONGODB_USER_DB", "users")
+	defer os.Setenv("MONGODB_USER_DB", originalValue)
+
+	repo := NewUserRepository(setup.Database)
+
+	user := model.NewUserDomain("test@mail.com", "test", "Eduardo", 46)
+	_, restErr := repo.CreateUser(user)
+	require.Nil(t, restErr, "Expected no error when creating user for find test")
+
+	dataToUpdate := model.NewUserDomain("newemail@mail.com", "newpassword", "Eduardo Lima", 47)
+
+	invalidID := "invalid-id-format"
+
+	restErr = repo.UpdateUser(invalidID, dataToUpdate)
+
+	require.NotNil(t, restErr, "Expected error when updating user with invalid ID")
+
+}
