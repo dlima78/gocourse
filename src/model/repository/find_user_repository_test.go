22935eac8c440c2bcb5fcffc0746621098c61beb
Eduardo <dlima78@hotmail.com>
2026-01-29 17:02:29 -0300

    Adicionar TestUserRepository_TestUpdateUserteste success

--- src/model/repository/find_user_repository_test.go
+++ src/model/repository/find_user_repository_test.go
@@ -9,15 +9,13 @@ import (
 )
 
 func TestUserRepository_FindUserByEmail(t *testing.T) {
-	// Setup MongoDB com testcontainers
 	setup := SetupMongoDB(t, "user_database_test")
 	defer setup.Cleanup()
 
-	// Setar a variável de ambiente para o nome da coleção
 	originalValue := os.Getenv("MONGODB_USER_DB")
 	os.Setenv("MONGODB_USER_DB", "users")
 	defer os.Setenv("MONGODB_USER_DB", originalValue)
-	// Criar repositório com a conexão real
+
 	repo := NewUserRepository(setup.Database)
 
 	user := model.NewUserDomain("test@mail.com", "test", "Eduardo", 46)
@@ -25,10 +23,8 @@ func TestUserRepository_FindUserByEmail(t *testing.T) {
 
 	require.Nil(t, restErr, "Expected no error when creating user for find test")
 
-	// Executar o teste de busca
 	result, restErr := repo.FindUserByEmail("test@mail.com")
 
-	// Assertions
 	require.Nil(t, restErr, "Expected no error when finding user by email")
 	require.NotNil(t, result, "Expected to find a user")
 	require.Equal(t, "test@mail.com", result.GetEmail(), "Expected email to match")
---
+++ src/model/repository/upadate_user_repository_test.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"os"
+	"testing"
+
+	"github.com/dlima78/gocourse/src/model"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUserRepository_TestUpdateUser(t *testing.T) {
+	setup := SetupMongoDB(t, "user_database_test")
+	defer setup.Cleanup()
+
+	originalValue := os.Getenv("MONGODB_USER_DB")
+	os.Setenv("MONGODB_USER_DB", "users")
+	defer os.Setenv("MONGODB_USER_DB", originalValue)
+
+	repo := NewUserRepository(setup.Database)
+
+	user := model.NewUserDomain("test@mail.com", "test", "Eduardo", 46)
+	createdUser, restErr := repo.CreateUser(user)
+	require.Nil(t, restErr, "Expected no error when creating user for find test")
+
+	dataToUpdate := model.NewUserDomain("newemail@mail.com", "newpassword", "Eduardo Lima", 47)
+
+	restErr = repo.UpdateUser(createdUser.GetID(), dataToUpdate)
+
+	require.Nil(t, restErr, "Expected no error when updating user")
+
+	updatedUser, restErr := repo.FindUserByID(createdUser.GetID())
+
+	require.Nil(t, restErr, "Expected no error when finding updated user")
+	require.Equal(t, "newemail@mail.com", updatedUser.GetEmail(), "Expected email to match")
+	require.Equal(t, "newpassword", updatedUser.GetPassword(), "Expected password to match")
+	require.Equal(t, "Eduardo Lima", updatedUser.GetName(), "Expected name to match")
+	require.Equal(t, int8(47), updatedUser.GetAge(), "Expected age to match")
+
+}
