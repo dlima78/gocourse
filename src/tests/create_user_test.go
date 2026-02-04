916c5f526206e64ebb1a61780099c6caf6e6086e
Eduardo <dlima78@hotmail.com>
2026-02-04 15:50:35 -0300

    Adicionar teste de integração TestCreatUser/duplicated_email_error

--- src/tests/create_user_test.go
+++ src/tests/create_user_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -13,6 +14,7 @@ import (
 	"github.com/dlima78/gocourse/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 func TestCreateUser(t *testing.T) {
@@ -35,6 +37,47 @@ func TestCreateUser(t *testing.T) {
 			"POST",
 			io.NopCloser(bytes.NewBufferString(string(body))))
 
+		UserController.CreateUser(ctx)
+
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+	})
+
+	t.Run("duplicated_email_error", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		ctx := mock_user_controller.GetTestingGinContext(recorder)
+
+		user := bson.M{
+			"email":    "duplicatedemail@mail.com",
+			"password": "TEste@123",
+			"name":     "João",
+			"age":      45,
+		}
+
+		_, err := Database.
+			Collection("test_user").
+			InsertOne(context.Background(), user)
+
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+
+		userRequest := request.UserRequest{
+			Email:    "duplicatedemail@mail.com",
+			Password: "TEste@123",
+			Name:     "João",
+			Age:      45,
+		}
+
+		body, _ := json.Marshal(userRequest)
+
+		mock_user_controller.MakeRequest(
+			ctx, gin.Params{},
+			url.Values{},
+			"POST",
+			io.NopCloser(bytes.NewBufferString(string(body))))
+
+		UserController.CreateUser(ctx)
 		UserController.CreateUser(ctx)
 
 		assert.Equal(t, http.StatusBadRequest, recorder.Code)
