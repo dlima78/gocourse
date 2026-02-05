cf36d1a1018052f43ec1eefddf84bb559d812412
Eduardo <dlima78@hotmail.com>
2026-02-05 14:49:06 -0300

    Adicionar teste de integração TestUpdateUser/success

--- src/tests/update_user_test.go
+++ src/tests/update_user_test.go
@@ -2,17 +2,21 @@ package test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 
 	mock_user_controller "github.com/dlima78/gocourse/src/controller/mocks"
 	"github.com/dlima78/gocourse/src/controller/model/request"
+	"github.com/dlima78/gocourse/src/model/repository/entity"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 func TestUpdateUser(t *testing.T) {
@@ -22,7 +26,7 @@ func TestUpdateUser(t *testing.T) {
 		id := "wrong-id"
 
 		userRequest := request.UserRequest{
-			Email:    "test@mail.com",
+			Email:    "wrongmail.com",
 			Password: "TEste@123",
 			Name:     "João",
 			Age:      45,
@@ -41,5 +45,55 @@ func TestUpdateUser(t *testing.T) {
 		UserController.UpdateUser(ctx)
 
 		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		ctx := mock_user_controller.GetTestingGinContext(recorder)
+		id := bson.NewObjectID()
+
+		_, err := Database.
+			Collection("test_user").
+			InsertOne(context.Background(), bson.M{
+				"_id":   id,
+				"email": "test@mail.com",
+				"name":  "Eduardo",
+				"age":   47,
+			})
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+
+		param := []gin.Param{{Key: "userId", Value: id.Hex()}}
+
+		userUpdateRequest := request.UserUpdateRequest{
+			Name: "João",
+			Age:  25,
+		}
+
+		b, _ := json.Marshal(userUpdateRequest)
+		stringReader := io.NopCloser(strings.NewReader(string(b)))
+
+		mock_user_controller.MakeRequest(
+			ctx,
+			param,
+			url.Values{},
+			"PUT",
+			stringReader,
+		)
+
+		UserController.UpdateUser(ctx)
+
+		assert.EqualValues(t, http.StatusOK, recorder.Result().StatusCode)
+
+		userEntity := entity.UserEntity{}
+		filter := bson.D{{Key: "_id", Value: id}}
+		_ = Database.
+			Collection("test_user").
+			FindOne(context.Background(), filter).Decode(&userEntity)
+
+		assert.EqualValues(t, userUpdateRequest.Name, userEntity.Name)
+		assert.EqualValues(t, userUpdateRequest.Age, userEntity.Age)
 	})
 }
