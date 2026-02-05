13c126f70b64494818b136d8dbe6309da695c138
Eduardo <dlima78@hotmail.com>
2026-02-05 15:40:23 -0300

    Adicionar teste de integração TestLoginUser/validation_error

---
+++ src/tests/login_user_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	mock_user_controller "github.com/dlima78/gocourse/src/controller/mocks"
+	"github.com/dlima78/gocourse/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoginUser(t *testing.T) {
+	t.Run("validation_error", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		ctx := mock_user_controller.GetTestingGinContext(recorder)
+
+		userLoginRequest := request.UserLoginRequest{
+			Email:    "test@mail.com",
+			Password: "wrongpassword",
+		}
+
+		body, _ := json.Marshal(userLoginRequest)
+		mock_user_controller.MakeRequest(
+			ctx,
+			gin.Params{},
+			url.Values{},
+			"POST",
+			io.NopCloser(bytes.NewBufferString(string(body))))
+
+		UserController.LoginUser(ctx)
+
+		assert.EqualValues(t, http.StatusBadRequest, recorder.Code)
+
+	})
+
+}
