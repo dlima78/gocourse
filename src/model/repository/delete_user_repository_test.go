0d306ccc17bf0d6c4aeb6bd97626f18ff3697039
Eduardo <dlima78@hotmail.com>
2026-02-05 19:49:05 -0300

    Refatorar TestUserRepository deleteUser

--- src/model/repository/delete_user_repository_test.go
+++ src/model/repository/delete_user_repository_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"net/http"
 	"os"
 	"testing"
 
@@ -10,42 +11,45 @@ import (
 )
 
 func TestUserRepository_DeleteUser(t *testing.T) {
-	setup := SetupMongoDB(t, "user_database_test")
-	defer setup.Cleanup()
+	t.Run("invalid_id", func(t *testing.T) {
+		setup := SetupMongoDB(t, "user_database_test")
+		defer setup.Cleanup()
 
-	originalValue := os.Getenv("MONGODB_USER_DB")
-	os.Setenv("MONGODB_USER_DB", "users")
-	defer os.Setenv("MONGODB_USER_DB", originalValue)
+		originalValue := os.Getenv("MONGODB_USER_DB")
+		os.Setenv("MONGODB_USER_DB", "users")
+		defer os.Setenv("MONGODB_USER_DB", originalValue)
 
-	repo := NewUserRepository(setup.Database)
+		repo := NewUserRepository(setup.Database)
 
-	user := model.NewUserDomain("test@mail.com", "test", "Eduardo", 46)
-	createdUser, _ := repo.CreateUser(user)
+		// Usar um ID inválido (não é um ObjectID válido)
+		invalidID := "invalid-id-format"
 
-	restErr := repo.DeleteUser(createdUser.GetID())
+		restErr := repo.DeleteUser(invalidID)
 
-	require.Nil(t, restErr, "Expected no error when deleting user")
+		require.NotNil(t, restErr, "Expected error when using invalid ID format")
+		assert.Equal(t, http.StatusBadRequest, restErr.Code, "Expected 400 error code for bad request")
 
-	result, restErr := repo.FindUserByEmail("test2@mail.com")
-	require.NotNil(t, restErr, "Expected error when finding non-existent user by email")
-	require.Nil(t, result, "Expected no user to be found for non-existent email")
-}
+	})
 
-func TestUserRepository_DeleteUser_InvalidID(t *testing.T) {
-	setup := SetupMongoDB(t, "user_database_test")
-	defer setup.Cleanup()
+	t.Run("success", func(t *testing.T) {
+		setup := SetupMongoDB(t, "user_database_test")
+		defer setup.Cleanup()
 
-	originalValue := os.Getenv("MONGODB_USER_DB")
-	os.Setenv("MONGODB_USER_DB", "users")
-	defer os.Setenv("MONGODB_USER_DB", originalValue)
+		originalValue := os.Getenv("MONGODB_USER_DB")
+		os.Setenv("MONGODB_USER_DB", "users")
+		defer os.Setenv("MONGODB_USER_DB", originalValue)
 
-	repo := NewUserRepository(setup.Database)
+		repo := NewUserRepository(setup.Database)
 
-	// Usar um ID inválido (não é um ObjectID válido)
-	invalidID := "invalid-id-format"
+		user := model.NewUserDomain("test@mail.com", "test", "Eduardo", 46)
+		createdUser, _ := repo.CreateUser(user)
 
-	restErr := repo.DeleteUser(invalidID)
+		restErr := repo.DeleteUser(createdUser.GetID())
 
-	require.NotNil(t, restErr, "Expected error when using invalid ID format")
-	assert.Equal(t, 400, restErr.Code, "Expected 400 error code for bad request")
+		require.Nil(t, restErr, "Expected no error when deleting user")
+
+		result, restErr := repo.FindUserByEmail("test2@mail.com")
+		require.NotNil(t, restErr, "Expected error when finding non-existent user by email")
+		require.Nil(t, result, "Expected no user to be found for non-existent email")
+	})
 }
