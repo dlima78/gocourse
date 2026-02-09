f956f5c52f5f4cfce6fc9cb8516b506bfc052ec7
Eduardo <dlima78@hotmail.com>
2026-02-09 13:54:14 -0300

    Adiconar teste de integração TestLoginUser/success

--- src/tests/login_user_test.go
+++ src/tests/login_user_test.go
@@ -2,7 +2,11 @@ package test
 
 import (
 	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -13,7 +17,16 @@ import (
 	"github.com/dlima78/gocourse/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
+
+// encryptPassword encripta a senha usando MD5 (mesma lógica do modelo)
+func encryptPassword(password string) string {
+	hash := md5.New()
+	defer hash.Reset()
+	hash.Write([]byte(password))
+	return hex.EncodeToString(hash.Sum(nil))
+}
 
 func TestLoginUser(t *testing.T) {
 	t.Run("validation_error", func(t *testing.T) {
@@ -58,6 +71,48 @@ func TestLoginUser(t *testing.T) {
 		UserController.LoginUser(ctx)
 
 		assert.EqualValues(t, http.StatusNotFound, recorder.Code)
+
+	})
+
+	t.Run("success", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		ctx := mock_user_controller.GetTestingGinContext(recorder)
+		id := bson.NewObjectID()
+
+		_, err := Database.
+			Collection("test_user").
+			InsertOne(context.Background(),
+				bson.M{
+					"_id":      id,
+					"email":    "test@mail.com",
+					"password": encryptPassword("Te@st13@"),
+					"name":     "Eduardo",
+					"age":      47,
+				})
+
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+
+		userLoginRequest := request.UserLoginRequest{
+			Email:    "test@mail.com",
+			Password: "Te@st13@",
+		}
+
+		body, _ := json.Marshal(userLoginRequest)
+
+		fmt.Println("BODY: ", string(body))
+		mock_user_controller.MakeRequest(
+			ctx,
+			gin.Params{},
+			url.Values{},
+			"POST",
+			io.NopCloser(bytes.NewBufferString(string(body))))
+
+		UserController.LoginUser(ctx)
+
+		assert.EqualValues(t, http.StatusOK, recorder.Code)
 
 	})
 
